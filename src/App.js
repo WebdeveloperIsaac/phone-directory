9bf70a5da84b82a47f3d92dd1a4721d7a3b327b6
Isaac <81617518+WebdeveloperIsaac@users.noreply.github.com>
2021-09-07 11:41:46 +0530

    Add files via upload
    React Updates

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{ Component } from'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        Phone DIrectory
+      </div>
+      <div>
+        <button >Add</button><br/>
+        <span>Name</span>
+        <span>phonenumber
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
